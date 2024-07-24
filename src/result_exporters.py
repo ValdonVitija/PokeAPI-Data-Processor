889e74eb1c81930326b18b57134a567060d45f5d
ValdonVitija <valdonvitijaa@gmail.com>
2024-07-24 06:01:54 +0200

    Fixed test result_exporters. Everytime tests ran, they modified the actual results of the original files.

--- src/result_exporters.py
+++ src/result_exporters.py
@@ -11,12 +11,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-VISUAL_RESULTS_PATH = f"{pathlib.Path(__file__).parent.parent}/results/visual"
-CSV_RESULTS_PATH = f"{pathlib.Path(__file__).parent.parent}/results/csv"
 
-
-def plot_avg_base_experience(avg_base_experience: pd.Series) -> None:
-    avg_base_experience.to_csv(f"{CSV_RESULTS_PATH}/avg_base_experience.csv")
+def plot_avg_base_experience(
+    avg_base_experience: pd.Series,
+    PATH=f"{pathlib.Path(__file__).parent.parent}/results",
+) -> None:
+    avg_base_experience.to_csv(f"{PATH}/csv/avg_base_experience.csv")
 
     plt.figure(figsize=(12, 6))
     sns.barplot(x=avg_base_experience.index, y=avg_base_experience.values)
@@ -25,12 +25,15 @@ def plot_avg_base_experience(avg_base_experience: pd.Series) -> None:
     plt.ylabel("Average Base Experience")
     plt.xticks(rotation=45)
     plt.tight_layout()
-    plt.savefig(f"{VISUAL_RESULTS_PATH}/avg_base_experience.png")
+    plt.savefig(f"{PATH}/visual/avg_base_experience.png")
     plt.close()
 
 
-def plot_unique_abilities_count(unique_abilities_count: pd.Series) -> None:
-    unique_abilities_count.to_csv(f"{CSV_RESULTS_PATH}/unique_abilities_count.csv")
+def plot_unique_abilities_count(
+    unique_abilities_count: pd.Series,
+    PATH=f"{pathlib.Path(__file__).parent.parent}/results",
+) -> None:
+    unique_abilities_count.to_csv(f"{PATH}/csv/unique_abilities_count.csv")
 
     plt.figure(figsize=(12, 6))
     sns.barplot(x=unique_abilities_count.index, y=unique_abilities_count.values)
@@ -39,14 +42,16 @@ def plot_unique_abilities_count(unique_abilities_count: pd.Series) -> None:
     plt.ylabel("Count of Unique Abilities")
     plt.xticks(rotation=45)
     plt.tight_layout()
-    plt.savefig(f"{VISUAL_RESULTS_PATH}/unique_abilities_count.png")
+    plt.savefig(f"{PATH}/visual/unique_abilities_count.png")
     plt.close()
 
 
-def plot_pokemon_group_counts(grouped_df: pd.DataFrame) -> None:
+def plot_pokemon_group_counts(
+    grouped_df: pd.DataFrame, PATH=f"{pathlib.Path(__file__).parent.parent}/results"
+) -> None:
     grouped_df["type_ability"] = grouped_df["types"] + " & " + grouped_df["abilities"]
 
-    grouped_df.to_csv(f"{CSV_RESULTS_PATH}/pokemon_group_counts.csv", index=False)
+    grouped_df.to_csv(f"{PATH}/csv/pokemon_group_counts.csv", index=False)
 
     filtered_df = grouped_df[grouped_df["count"] > 0]
     filtered_df.reset_index(drop=True, inplace=True)
@@ -69,4 +74,4 @@ def plot_pokemon_group_counts(grouped_df: pd.DataFrame) -> None:
         title_x=0.5,
     )
 
-    fig.write_html(f"{VISUAL_RESULTS_PATH}/pokemon_group_counts.html")
+    fig.write_html(f"{PATH}/visual/pokemon_group_counts.html")
--- tests/test_result_exporters.py
+++ tests/test_result_exporters.py
@@ -6,6 +6,7 @@ to generate both formats, by only testing the csv generation we assure that the 
 """
 
 import pytest
+from unittest.mock import patch
 import pandas as pd
 from pathlib import Path
 from src.result_exporters import (
@@ -15,30 +16,30 @@ from src.result_exporters import (
 )
 
 # NOTE: This folder contains csv files generated from the pokemons we receive on the first pagination of the api.
-CSV_RESULTS_PATH = f"{Path(__file__).parent}/results/csv"
+CSV_RESULTS_PATH = f"{Path(__file__).parent}/results"
 
 
 def test_plot_avg_base_experience(expected_avg_base_experience):
-    plot_avg_base_experience(expected_avg_base_experience)
+    plot_avg_base_experience(expected_avg_base_experience, CSV_RESULTS_PATH)
     expected_df = expected_avg_base_experience.to_frame()
-    output_file = Path(f"{CSV_RESULTS_PATH}/avg_base_experience.csv")
+    output_file = Path(f"{CSV_RESULTS_PATH}/csv/avg_base_experience.csv")
     assert output_file.exists()
     result_df = pd.read_csv(output_file, index_col=0)
     pd.testing.assert_frame_equal(result_df, expected_df)
 
 
 def test_plot_unique_abilities_count(expected_unique_abilities_count):
-    plot_unique_abilities_count(expected_unique_abilities_count)
+    plot_unique_abilities_count(expected_unique_abilities_count, CSV_RESULTS_PATH)
     expected_df = expected_unique_abilities_count.to_frame()
-    output_file = Path(f"{CSV_RESULTS_PATH}/unique_abilities_count.csv")
+    output_file = Path(f"{CSV_RESULTS_PATH}/csv/unique_abilities_count.csv")
     assert output_file.exists()
     result_df = pd.read_csv(output_file, index_col=0)
     pd.testing.assert_frame_equal(result_df, expected_df)
 
 
 def test_plot_pokemon_group_counts(expected_grouped_df):
-    plot_pokemon_group_counts(expected_grouped_df)
-    output_file = Path(f"{CSV_RESULTS_PATH}/pokemon_group_counts.csv")
+    plot_pokemon_group_counts(expected_grouped_df, CSV_RESULTS_PATH)
+    output_file = Path(f"{CSV_RESULTS_PATH}/csv/pokemon_group_counts.csv")
     assert output_file.exists()
     result_df = pd.read_csv(output_file)
     pd.testing.assert_frame_equal(result_df, expected_grouped_df)
